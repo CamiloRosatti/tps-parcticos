666ebd3a38a564b6aeb5c5753da031764e86609a
CamiloRosatti <166017290+CamiloRosatti@users.noreply.github.com>
2024-04-04 08:30:38 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+console.log("la suma es"+(1+1))
+console.log("la resta es"+(2-1))
+console.log("la multiplicacion es"+(3*3))
+console.log("la division es"+(4/2))
+
+let numero=2
+let numero=1
+let numero=9
+let numero=2
+
+alert(numero1)
+alert(numero2)
+alert(numero3)
+alert(numero4)
+
+let añoActual=prompt("ingresa el año actual")
+let añoNacimiento=prompt("ingresa tu año de nacimiento")
+
+alert("la edad es:"+(2024-2009))
+let nombre= prompt("ingresa tu nombre")
+alert("bienvenido" + nombre)
+let nombreAlumno= "Camilo"
+let apellidoAlumno= "Rosatti"
+console.log("bienvenido"+ let.nombreAlumno+let.apellidoAlumno)
+alert("bienvenido"+ let.nombreAlumno+let.apellidoAlumno)
+let diez=10
+let tres=3
+let resultadoMultiplicacion= 10*3
+alert("el resultado de la multiplicacion es"+ 10*3)
